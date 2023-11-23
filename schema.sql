19110c81d5bc9681a357cb5cea86190366c2102f
Allanpada <133250185+Allanpada@users.noreply.github.com>
2023-11-23 01:47:02 +0000

    schema

---
+++ schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE perguntas;
+
+USE perguntas;
+
+CREATE TABLE quizzies(
+    id INT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+
+);
+
+CREATE TABLE questoes(
+    id INT PRIMARY KEY,
+    enunciado varchar(255) NOT NULL,
+    id_quiz INT NOT NULL,
+    FOREIGN KEY (id_quiz) REFERENCES quizzes(id)
+);
+)CREATE TABLE alternativas(
+    id INT PRIMARY KEY,
+    texto varchar(255) NOT NULL,
+    id_questao INT NOT NULL,
+    FOREIGN KEY (id_questao) REFERENCES questoes(id)
+);
